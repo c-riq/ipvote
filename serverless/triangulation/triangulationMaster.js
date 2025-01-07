b259ee6b69f8f1bd212d5f81940d7b362451c980
c-riq <chrisrieck6@gmail.com>
2025-01-07 17:36:27 +0100

    Add latency triangulation proxies

--- serverless/triangulation/triangulationMaster.js
+++ serverless/triangulation/triangulationMaster.js
@@ -1,12 +1,16 @@
 // AWS lambda node.js to respond with random nonce
 // record timestamp and store in S3 along with nonce and IP address
-const { S3Client, GetObjectCommand, PutObjectCommand, ListObjectsV2Command } = require('@aws-sdk/client-s3');
+const { S3Client, PutObjectCommand, ListObjectsV2Command } = require('@aws-sdk/client-s3');
+
+const awsRegionOfMaster = 'us-east-1';
 
 exports.handler = async (event) => {
     const arrival_timestamp = new Date().getTime();
 
     const proxyId = event.queryStringParameters?.proxyId;
     const nonce = event.queryStringParameters?.nonce;
+    const clientStartTimestamp = event.queryStringParameters?.clientStartTimestamp;
+    const clientReceivedNonceTimestamp = event.queryStringParameters?.clientReceivedNonceTimestamp;
     const ip = event.requestContext.http.sourceIp;
 
     if (!proxyId && !nonce) {
@@ -19,8 +23,8 @@ exports.handler = async (event) => {
         const s3 = new S3Client({ region: 'us-east-1' });
         const command = new PutObjectCommand({
             Bucket: 'ipvotes',
-            Key: `${nonce}-1.json`,
-            Body: JSON.stringify({ event: 'nonceGeneration',nonce, ip, timestamp: arrival_timestamp })
+            Key: `triangulation/${nonce}-1.json`,
+            Body: JSON.stringify({ event: 'nonceGeneration',nonce, ip, timestamp: arrival_timestamp, awsRegionOfMaster })
 
         });
         s3.send(command);
@@ -52,8 +56,11 @@ exports.handler = async (event) => {
 
         const putCommand = new PutObjectCommand({
             Bucket: 'ipvotes',
-            Key: `${nonce}-${proxyId}.json`,
-            Body: JSON.stringify({ event: 'proxyRequestReceived',nonce, ip, timestamp: arrival_timestamp })
+            Key: `triangulation/${nonce}-${proxyId}.json`,
+            Body: JSON.stringify({ event: 'proxyRequestReceived', nonce, ip, 
+                timestamp: arrival_timestamp, awsRegionOfMaster, proxyId,
+                clientStartTimestamp, clientReceivedNonceTimestamp
+            })
         });
         await s3.send(putCommand);  
 
