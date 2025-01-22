d17ce999268265254e6de7c2476a011818243d45
c-riq <chrisrieck6@gmail.com>
2025-01-22 16:07:06 +0100

    Fix types

--- frontend/src/components/ui/Geolocation.tsx
+++ frontend/src/components/ui/Geolocation.tsx
@@ -785,7 +785,7 @@ function Geolocation({ privacyAccepted, userIpInfo, onPrivacyAcceptChange, captc
         accepted={privacyAccepted}
         onAcceptChange={onPrivacyAcceptChange}
         textAlign="center"
-        captchaToken={'_'}
+        captchaToken={captchaToken || '_'}
         setCaptchaToken={setCaptchaToken}
       />
 
