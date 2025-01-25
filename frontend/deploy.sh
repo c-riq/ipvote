8cf2a177e6bd3a26d2b7d05fd2c4fd3be627d11c
c-riq <chrisrieck6@gmail.com>
2025-01-25 18:09:44 +0100

    Adjust caching

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -32,8 +32,12 @@ fi
 # Rename index.html with random string
 mv dist/index.html "dist/index_${random_string}.html"
 
+# Sync all files except HTML with long cache duration
 echo Synching Build Folder: $s3_bucket...
-aws s3 sync dist/ s3://$s3_bucket --delete --cache-control max-age=31530000,public
+aws s3 sync dist/ s3://$s3_bucket --delete --exclude "*.html" --cache-control max-age=31530000,public
+
+# Upload HTML file with no-cache headers
+aws s3 cp "dist/index_${random_string}.html" "s3://$s3_bucket/index_${random_string}.html" --cache-control "no-cache, no-store, must-revalidate"
 
 # Update CloudFront function or origin response policy to use the new index file
 if [ ! -z "$cf_id" ]; then
